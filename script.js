8581d7abd8f41fb4498580b05ee2bcb70029a767
Rupak Deb Barman <71335076+RDBarman2001@users.noreply.github.com>
2023-04-23 20:51:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function updatePrice() {
+  var price = Math.floor(Math.random() * 20); // Generate random number between 20 and 30
+  document.getElementById("dam").innerHTML = "Price" + ":" + "$" + price;
+  console.log(price);
+}
+updatePrice(); // Initial price update on website
